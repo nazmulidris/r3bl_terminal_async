6472826ecfb6bed359a056d885d70450fe8ed083
Nazmul Idris <nazmul@fasterlap.com>
2024-04-12 16:46:41 -0500

    Update Braille spinner animation
    Fixes: #30

--- src/spinner_impl/spinner_constants.rs
+++ src/spinner_impl/spinner_constants.rs
@@ -17,12 +17,14 @@
 
 use std::time::Duration;
 
-pub const DELAY_MS: u64 = 100;
+pub const DELAY_MS: u64 = 85;
 pub const DELAY_UNIT: Duration = Duration::from_millis(DELAY_MS);
-pub const ARTIFICIAL_UI_DELAY: Duration = Duration::from_millis(DELAY_MS * 20);
-pub const BRAILLE_DOTS: [&str; 30] = [
-    "⠁", "⠁", "⠉", "⠙", "⠚", "⠒", "⠂", "⠂", "⠒", "⠲", "⠴", "⠤", "⠄", "⠄", "⠤", "⠠", "⠠", "⠤", "⠦",
-    "⠖", "⠒", "⠐", "⠐", "⠒", "⠓", "⠋", "⠉", "⠈", "⠈", " ",
+pub const ARTIFICIAL_UI_DELAY: Duration = Duration::from_millis(DELAY_MS * 40);
+
+/// More info: <https://www.unicode.org/charts/script/chart_Braille.html>
+pub const BRAILLE_DOTS: [&str; 34] = [
+    "⠁", "⠃", "⡇", "⠇", "⡎", "⢟", "⡯", "⡗", "⡞", "⡟", "⡷", "⡾", "⡾", "⣕", "⣗", "⣝", "⡣", "⡮", "⡯",
+    "⡳", "⡵", "⣞", "⣟", "⣧", "⣮", "⣯", "⣷", "⣿", "⣼", "⡟", "⡏", "⠇", "⠃", "⠁",
 ];
 
 pub const BLOCK_DOTS: [&str; 8] = ["█", "▓", "▒", "░", "░", "▒", "▓", "█"];
