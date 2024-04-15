55f234200422ae81f1d3b981da9448f653f803f7
Nazmul Idris <nazmul@fasterlap.com>
2024-04-15 10:52:58 -0500

    Update examples and add tree walking command
    Fixes: #3

--- examples/terminal_async.rs
+++ examples/terminal_async.rs
@@ -40,6 +40,9 @@ enum Command {
     Spinner,
 
     #[strum(ascii_case_insensitive)]
+    Tree,
+
+    #[strum(ascii_case_insensitive)]
     StartTask1,
 
     #[strum(ascii_case_insensitive)]
@@ -72,8 +75,9 @@ fn get_info_message() -> String {
         format!("{:?}", commands).blue()
     };
     let info_message = format!(
-        "try Ctrl+D, Up, Down, Left, Right, Ctrl+left, Ctrl+right, `{}`, `{}`, `{}`, and `{}`",
+        "try Ctrl+D, Up, Down, Left, Right, Ctrl+left, Ctrl+right, `{}`, `{}`, `{}`, `{}`, and `{}`",
         Command::StartTask1,
+        Command::Tree,
         Command::Spinner,
         Command::StartTask2,
         Command::StopPrintouts
@@ -310,6 +314,23 @@ mod process_input_event {
                         Duration::from_millis(100),
                     );
                 }
+                Command::Tree => {
+                    let mut shared_writer_clone = shared_writer.clone();
+                    tokio::spawn(async move {
+                        let mut_shared_writer = &mut shared_writer_clone;
+                        match file_walker::get_current_working_directory() {
+                            Ok((root_path, _)) => {
+                                match file_walker::display_tree(root_path, mut_shared_writer, true)
+                                    .await
+                                {
+                                    Ok(_) => {}
+                                    Err(_) => todo!(),
+                                };
+                            }
+                            Err(_) => todo!(),
+                        }
+                    });
+                }
             },
         }
 
@@ -376,5 +397,206 @@ mod long_running_task {
                 let _ = spinner.stop(msg.as_str()).await;
             }
         });
+    }
+}
+
+pub mod file_walker {
+    use super::*;
+    use miette::miette;
+    use r3bl_terminal_async::SendRawTerminal;
+    use std::{
+        fs,
+        path::{self, PathBuf},
+    };
+
+    pub const FOLDER_DELIM: &str = std::path::MAIN_SEPARATOR_STR;
+    pub const SPACE_CHAR: &str = " ";
+    pub const INDENT_MULTIPLIER: usize = 4;
+
+    /// - Get the current working directory. Eg:
+    ///   `/home/nazmul/github/r3bl_terminal_async`.
+    /// - Returns a tuple of `(path, name)`. Eg:
+    ///   (`/home/nazmul/github/r3bl_terminal_async`, `r3bl_terminal_async`).
+    pub fn get_current_working_directory() -> miette::Result<(/*path*/ String, /*name*/ String)> {
+        let path = std::env::current_dir().into_diagnostic()?;
+
+        let name = path
+            .file_name()
+            .ok_or_else(|| miette!("Could not get current working directory"))?;
+
+        Ok((
+            path.to_string_lossy().to_string(),
+            name.to_string_lossy().to_string(),
+        ))
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Folder {
+        name: String,
+        full_path: String,
+        depth: Option<usize>,
+    }
+
+    pub fn create_root(root_path: String) -> miette::Result<Folder> {
+        // Validate that root_path is a directory.
+        let metadata = fs::metadata(&root_path).into_diagnostic()?;
+        if !metadata.is_dir() {
+            return Err(miette!("The path is not a directory"));
+        }
+
+        // Get the folder name.
+        let folder = PathBuf::from(&root_path);
+        let name = folder
+            .file_name()
+            .ok_or_else(|| miette!("The root's full path is not a directory: {}", root_path))?;
+
+        Ok(Folder {
+            name: name.to_string_lossy().to_string(),
+            full_path: root_path,
+            depth: Some(0),
+        })
+    }
+
+    pub async fn create_child_and_add_to(
+        parent_node: &mut Folder,
+        child_name: String,
+    ) -> miette::Result<Folder> {
+        let child_full_path = format!("{}{FOLDER_DELIM}{child_name}", parent_node.full_path);
+
+        // Validate that child's new full path is a directory.
+        let metadata = fs::metadata(&child_full_path).into_diagnostic()?;
+        if !metadata.is_dir() {
+            return Err(miette!(
+                "The child's new full path is not a directory: {}",
+                child_full_path
+            ));
+        }
+
+        Ok(Folder {
+            name: child_name,
+            full_path: child_full_path,
+            depth: Some(parent_node.depth.unwrap_or(0) + 1),
+        })
+    }
+
+    /// Walk the current working directory. Display a tree formatted view of the
+    /// sub-folders just like the `tree` command on Linux. This algorithm is a depth-first
+    /// search (DFS) algorithm, which relies on the [fs], which is a tree, to get a list
+    /// of folders (not files) contained in any given folder. There's no need to
+    /// explicitly construct a tree, since the [fs] is a non-binary tree.
+    ///
+    /// The [fs::read_dir] function determines what the order of the traversal is.
+    /// Non-binary trees, also known as N-ary trees, can be traversed in several ways.
+    /// Here are the most common types of non-binary tree traversals: Depth-First Search
+    /// (DFS): This traversal method explores as far as possible along each branch before
+    /// backtracking. DFS itself can be further divided into three types:
+    ///
+    /// 1) Pre-Order Traversal: In this traversal method, the root is visited first, then
+    /// the left subtree, and finally the right subtree.
+    ///
+    /// 2) In-Order Traversal: In this traversal method, the left subtree is visited
+    /// first, then the root, and finally the right subtree. Note: This method is only for
+    /// binary trees.
+    ///
+    /// 3) Post-Order Traversal: In this traversal method, the left subtree is visited
+    /// first, then the right subtree, and finally the root. Breadth-First Search (BFS):
+    /// Also known as level-order traversal, this method visits all the nodes of a level
+    /// before going to the next level.
+    ///
+    /// 4) Spiral/Zigzag Order Traversal: This is a variant of BFS. In this traversal,
+    /// levels are visited in alternating left-to-right and right-to-left order.
+    ///
+    /// Remember, the "left" and "right" in these traversal methods are just for
+    /// explanation purposes. In a non-binary tree, a node can have more than two
+    /// children. So, in actual implementation, "left" and "right" can be replaced with
+    /// "first child", "second child", "third child", and so on.
+    ///
+    /// More info:
+    /// 1. <https://developerlife.com/2018/08/16/algorithms-in-kotlin-3/>
+    /// 2. <https://developerlife.com/2022/02/24/rust-non-binary-tree/>
+    /// 3. <https://developerlife.com/2022/12/11/algo-ts-2/>
+    pub async fn display_tree(
+        root_path: String,
+        shared_writer: &mut SharedWriter,
+        delay_enable: bool,
+    ) -> miette::Result<()> {
+        let root = create_root(root_path)?;
+
+        // Walk the root.
+        let mut stack = vec![root.clone()];
+
+        while let Some(mut current_node) = stack.pop() {
+            // Print the current node.
+            print_node(shared_writer, &current_node)?;
+            if delay_enable {
+                tokio::time::sleep(Duration::from_millis(10)).await;
+            }
+
+            // Add node's sub-folders to the stack.
+            let current_full_path = path::Path::new(&current_node.full_path);
+
+            // Only get a list of folders (not files) contained in the node by scanning
+            // the file system. We didn't specify the sorting order, so it's up to the
+            // operating system to decide the order. This affects the ordering mentioned
+            // in the docs above.
+            let vec_folder_name: Vec<String> = fs::read_dir(current_full_path)
+                .into_diagnostic()?
+                .filter_map(Result::ok)
+                .filter(|entry| entry.path().is_dir())
+                .map(|entry| entry.file_name().to_string_lossy().to_string())
+                .collect();
+
+            // Add each sub-folder (contained in the current node) to the stack. And add
+            // it to the current node's children.
+            for sub_folder_name in vec_folder_name {
+                stack.push(create_child_and_add_to(&mut current_node, sub_folder_name).await?);
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn print_node(writer: &mut SendRawTerminal, node: &Folder) -> miette::Result<()> {
+        writeln!(
+            writer,
+            "{}{}",
+            SPACE_CHAR.repeat(INDENT_MULTIPLIER * node.depth.unwrap_or(0)),
+            node.name
+        )
+        .into_diagnostic()?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_display_tree() -> miette::Result<()> {
+        let (line_sender, mut line_receiver) = tokio::sync::mpsc::channel(1_000);
+        let mut shared_writer = SharedWriter {
+            buffer: Vec::new(),
+            line_sender,
+        };
+
+        let (path, _) = get_current_working_directory()?;
+
+        display_tree(path, &mut shared_writer, false).await.unwrap();
+
+        assert_eq!(shared_writer.buffer.len(), 0);
+
+        // Print everything in line_receiver.
+        let mut output_lines = vec![];
+        loop {
+            let it = line_receiver.try_recv().into_diagnostic();
+            match it {
+                Ok(LineControlSignal::Line(it)) => {
+                    output_lines.push(String::from_utf8_lossy(&it).to_string());
+                    print!("{}", String::from_utf8_lossy(&it));
+                }
+                _ => break,
+            }
+        }
+
+        assert_ne!(output_lines.len(), 0);
+
+        Ok(())
     }
 }
